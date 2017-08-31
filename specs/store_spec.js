d6755f91f7642d3479ca57e785e944cad5d30c3e
MartyCodeClan <zabbalab@hotmail.com>
2017-08-31 17:33:04 +0100

    count and add record methods created

--- specs/store_spec.js
+++ specs/store_spec.js
@@ -4,16 +4,26 @@ var Record = require("../record.js");
 
 describe("Park", function(){
 
-  var store1;
+  var store;
   var record1;
   var record2;
   var record3;
 
   beforeEach(function(){
-    store1 = new Store("A&B Sound", "Vancouver", 0);
+    store = new Store("A&B Sound", "Vancouver", 0);
     // how to start with records in inventory array?
     record1 = new Record("Guns n' Roses", "Appetite for Destruction", "Rock", 12);
     record2 = new Record("Beck", "Odelay", "Alternative", 15);
     record3 = new Record("Hot Dreams", "Timber Timbre", "Folk Rock", 10);
+  });
+
+  it("inventory count counts 0", function(){
+    assert.strictEqual(store.inventoryCount(), 0);
+  });
+
+  it("can add record to inventory", function(){
+    store.addRecord(record1);
+    store.addRecord(record2);
+    assert.strictEqual(store.inventoryCount(), 2);
   });
 });
--- store.js
+++ store.js
@@ -1,8 +1,16 @@
-var Store = function(store, city, balace) {
+var Store = function(store, city, balance) {
   this.store = store;
   this.city = city;
   this.balance = balance;
   this.inventory = [];
 }
+
+  Store.prototype.inventoryCount = function(){
+    return this.inventory.length;
+  };
+
+  Store.prototype.addRecord = function(record){
+    this.inventory.push(record);
+  };
 
 module.exports = Store;
